68d3e51c5126cd05bdb08c2c06526744a0c355c1
Israel Sant'Anna <israel.santanna@gmail.com>
2022-09-07 15:03:06 -0300

    feat(components): export all components from `index.ts`

---
+++ app/components/index.ts
@@ -0,0 +1,11 @@
+import Divider from './Divider'
+import Loader from './Loader'
+import MonetaryValueDisplay from './MonetaryValueDisplay'
+import ScrollableContainer from './ScrollableContainer'
+
+export {
+  Divider,
+  Loader,
+  MonetaryValueDisplay,
+  ScrollableContainer
+}

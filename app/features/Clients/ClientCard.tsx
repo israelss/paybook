3a2694c4a9f812c92c3193a7787a084d18d938ac
Israel Sant'Anna <israel.santanna@gmail.com>
2022-09-07 16:44:34 -0300

    feat(features/Clients): create `ClientCard` component

---
+++ app/features/Clients/ClientCard.tsx
@@ -0,0 +1,39 @@
+import { DebtsUtils } from '../Debts'
+import { Divider, MonetaryValueDisplay } from '~/components'
+import { memo } from 'react'
+import { NavLink, useParams } from '@remix-run/react'
+import type { ClientCardProps } from './types'
+
+const ClientCard = ({ client }: ClientCardProps): JSX.Element => {
+  const { id } = useParams()
+
+  const clientTotalDebt = DebtsUtils.sumDebtsValues(
+    client.debts.filter(debt => debt.paymentDate === null)
+  )
+
+  const clientSelected = client.id === id
+
+  return (
+    <div className='sticky top-0 pt-2 bg-base-100'>
+      <NavLink
+        className={`flex flex-col p-4 pb-2 rounded-lg items-start scroll-mt-2 ${clientSelected ? 'bg-info-content text-info' : 'bg-neutral text-neutral-content'}`}
+        to={!clientSelected ? client.id : '.'}
+      >
+        <span>{client.name}</span>
+        <Divider />
+        <div className='flex items-center justify-between w-full gap-5'>
+          <span className={`text-sm ${clientSelected ? 'text-info' : 'text-neutral-content'}`}>
+            Total
+          </span>
+          <MonetaryValueDisplay value={clientTotalDebt} />
+        </div>
+        <Divider />
+        <div className={`text-xs ${clientSelected ? 'text-warning' : 'text-info'}`}>
+          {clientSelected ? 'Ocultar ' : 'Ver '} Detalhes
+        </div>
+      </NavLink>
+    </div>
+  )
+}
+
+export default memo(ClientCard)

71662ede55a50e7d551920128a52c20fa1bc2898
Israel Sant'Anna <israel.santanna@gmail.com>
2022-09-07 14:59:46 -0300

    feat(components): create `Loader` component

---
+++ app/components/Loader.tsx
@@ -0,0 +1,10 @@
+const Loader = (): JSX.Element => {
+  return (
+    <div className='flex flex-col items-center justify-center w-screen h-screen gap-3'>
+      <h2 className='text-3xl text-center'>Carregando dados...</h2>
+      <progress className='w-full max-w-sm px-4 progress' />
+    </div>
+  )
+}
+
+export default Loader

ee522e94d3006aae29a70a787955c2fd1494bf30
Israel Sant'Anna <israel.santanna@gmail.com>
2022-09-07 16:50:29 -0300

    feat(features/Debts): export from `index.ts`
    Export:
     - `DebtCard` component

--- app/features/Debts/index.ts
+++ app/features/Debts/index.ts
@@ -1,4 +1,10 @@
+import DebtCard from './DebtCard'
+
 export * as DebtsActions from './actions'
 export * as DebtsApi from './api'
 export * as DebtsTypes from './types'
 export * as DebtsUtils from './utils'
+
+export {
+  DebtCard
+}

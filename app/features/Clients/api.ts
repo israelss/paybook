3637d8d17b614b25befc69d4dc10e31b4751020a
Israel Sant'Anna <israel.santanna@gmail.com>
2022-09-08 15:30:21 -0300

    feat(features/Clients/api): create `getClientIdByName`

--- app/features/Clients/api.ts
+++ app/features/Clients/api.ts
@@ -1,6 +1,7 @@
 import { db } from '~/utils/db.server'
 import { sortClientsByDebts } from './utils'
 import type { AllClients } from './types'
+import invariant from 'tiny-invariant'
 
 export const getAll = async (): Promise<AllClients> => {
   const clients = await db.client.findMany({
@@ -17,6 +18,19 @@ export const getAll = async (): Promise<AllClients> => {
   clients.sort(sortClientsByDebts)
 
   return { clients }
+}
+
+export const getClientIdByName = async (name: string): Promise<string> => {
+  const client = await db.client.upsert({
+    where: { name },
+    select: { id: true },
+    update: {},
+    create: { name }
+  })
+
+  invariant(client, 'Client not found!')
+
+  return client.id
 }
 
 export const remove = async (id: string): Promise<void> => {

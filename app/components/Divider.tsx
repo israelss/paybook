2d1dcc956238a453deace00635b6d06f1de43e24
Israel Sant'Anna <israel.santanna@gmail.com>
2022-09-10 16:01:11 -0300

    refactor(components/Divider): use more semantic tag

--- app/components/Divider.tsx
+++ app/components/Divider.tsx
@@ -1,9 +1,18 @@
+import React from 'react'
+
 interface DividerProps {
   horizontal?: boolean
 }
 
 const Divider = ({ horizontal = false }: DividerProps): JSX.Element => {
-  return <div className={`divider after:bg-base-300 before:bg-base-300 ${horizontal ? 'mx-0 divider-horizontal' : 'my-0'}`} />
+  return (
+    <hr
+      className={`
+        divider border-t-0 after:bg-base-300 before:bg-base-300
+        ${horizontal ? 'mx-0 divider-horizontal' : 'my-0'}
+      `}
+    />
+  )
 }
 
 export default Divider
